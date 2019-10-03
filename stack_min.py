91316f6beda220b2f8deed9acb6103ade0870e52
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2019-10-03 14:22:03 -0700

    added doctest function

--- stack_min.py
+++ stack_min.py
@@ -6,9 +6,12 @@ class Stack():
         self.min = []
 
     def push(self, item):
-        """ append item to stack. 
-        If items exist in min append item only if less than the last element in min.
-        elif add min  """
+        """ 
+        append item to stack. 
+        If min is empty
+            append to item to min
+        elif item less than or equal to last element in min
+            append item to min """
         self._stack.append(item)
 
         if self.min == []:
@@ -18,18 +21,38 @@ class Stack():
             self.min.append(item)
 
     def pop(self):
+        """If the most recently added element in min is equal to popped item. 
+                Pop item from min"""
         popped_item = self._stack.pop()
         if self.min[-1] == popped_item:
             self.min.pop()
 
-    def min(self):
+    def min_value(self):
         return self.min[-1]
 
-    def peek(self)
+    def peek(self):
         return self._stack[0]
 
-    def isEmpty(self)
+    def isEmpty(self):
         if len(self._stack) == 0:
             return True
         else:
             return False
+
+
+myNewStack=Stack()
+
+myNewStack.push(7)
+myNewStack.push(8)
+myNewStack.push(1)
+myNewStack.push(2)
+print(myNewStack.min_value())
+myNewStack.pop()
+print(myNewStack.min_value())
+myNewStack.pop()
+print(myNewStack.min_value())
+
+if __name__='__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("passed all tests! Booya!!")
