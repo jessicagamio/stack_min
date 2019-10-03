fcb863e1baa04125ea828dd8e5c6ea330424fe08
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2019-10-03 14:12:16 -0700

    correcting push function

--- stack_min.py
+++ stack_min.py
@@ -6,14 +6,24 @@ class Stack():
         self.min = []
 
     def push(self, item):
+        """ append item to stack. 
+        If items exist in min append item only if less than the last element in min.
+        elif add min  """
         self._stack.append(item)
-        if self.min and item <= self.min[-1]:
+
+        if self.min == []:
+            self.min.append(item)
+
+        elif item <= self.min[-1]:
             self.min.append(item)
 
     def pop(self):
         popped_item = self._stack.pop()
         if self.min[-1] == popped_item:
             self.min.pop()
+
+    def min(self):
+        return self.min[-1]
 
     def peek(self)
         return self._stack[0]
